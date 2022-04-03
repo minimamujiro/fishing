ac382d7e74b0f12ab4c71e099464e3afb09e7232
minimamujiro <minimamujiro@yahoo.co.jp>
2022-04-03 18:37:48 +0900

    first commit

---
+++ src/main/java/com/example/fishing/FishingApplication.java
@@ -0,0 +1,13 @@
+package com.example.fishing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fishing/FishingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fishing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
