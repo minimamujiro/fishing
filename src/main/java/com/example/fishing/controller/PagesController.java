9314eed2fceebc1ff7ad99a9c0dd586e5b14505d
minimamujiro <minimamujiro@yahoo.co.jp>
2022-05-16 18:59:36 +0900

    更新

--- src/main/java/com/example/fishing/controller/PagesController.java
+++ src/main/java/com/example/fishing/controller/PagesController.java
@@ -14,10 +14,10 @@ import com.example.fishing.service.S3Wrapper;
 
 @Controller
 public class PagesController {
-	
+
 	@Autowired
 	S3Wrapper s3;
-	
+
 	@RequestMapping("/")
 	public String index(Model model) throws IOException {
 		/*アフィリエイトの表示*/
@@ -25,13 +25,14 @@ public class PagesController {
 		ResponseEntity<byte[]> entity = s3.download("tags");
 		String body = new String(entity.getBody());
 		model.addAttribute("tags", body.split(System.getProperty("line.separator")));
+		/*pages/indexに潮汐APIで使う値を渡す*/
 		Date now = new Date();
-	    String year = new SimpleDateFormat("yyyy").format(now);
-	    String month = new SimpleDateFormat("M").format(now);
-	    String day = new SimpleDateFormat("d").format(now);
-	    model.addAttribute("year", year);
-	    model.addAttribute("month", month);
-	    model.addAttribute("day", day);
+		String year = new SimpleDateFormat("yyyy").format(now);
+		String month = new SimpleDateFormat("M").format(now);
+		String day = new SimpleDateFormat("d").format(now);
+		model.addAttribute("year", year);
+		model.addAttribute("month", month);
+		model.addAttribute("day", day);
 		return "pages/index";
 	}
 }
